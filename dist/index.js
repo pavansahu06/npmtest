9008baf9616bec789f6892ad96480db6eb8c3d6b
Pavan Sahu <pavansahu06@gmail.com>
2019-12-31 10:23:08 +0530

    adding final codes for package registry

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class BasicMath {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    add() {
+        return this.a + this.b;
+    }
+}
+exports.default = BasicMath;
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,9 @@
+class BasicMath {
+    constructor(private a: number, private b: number){}
+
+    add()
+    {
+        return this.a +this.b;
+    }
+}
+export default BasicMath;
